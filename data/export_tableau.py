68ae3cb54dd5f304a077092a5a05153e687081ba
Adhishree22 <adhishree2207@gmail.com>
2026-05-21 08:05:54 +0000

    Added

--- data/export_tableau.py
+++ data/export_tableau.py
@@ -19,7 +19,7 @@ def scale_numeric_columns(df, skip_cols):
 
 
 def sort_dataframe(df):
-  
+
   df = df.copy()
 
   # If Year exists as a column
@@ -39,27 +39,27 @@ def prepare_year_column(df):
 
         # Rename created index column to Year
         df.rename(columns={"index": "Year"}, inplace=True)
-		
+
     # If DatetimeIndex exists
     elif isinstance(df.index, pd.DatetimeIndex):
 
         df["Year"] = df.index.year
         df = df.reset_index(drop=True)
-		
+
     return df
-	
-	
+
+
 def export_single_dataframe(df, filename, skip_cols=None):
 
   # If no skip columns provided
   if skip_cols is None:
     skip_cols = []
-	
+
   df = scale_numeric_columns(df, skip_cols)
   df = prepare_year_column(df)
   df = sort_dataframe(df)
 
-  path = f"dashboard/{filename}.csv"
+  path = f"dashboard/csvs/{filename}.csv"
 
   df.to_csv(path, index=False)
 
@@ -93,27 +93,27 @@ def export_for_tableau(
 
   full_skip = ['Closing', 'Shares', 'EPS', 'Check',"DPS"]
   exported["full_df"] = export_single_dataframe(full_df, "full_df", full_skip)
-  
+
   growth_skip = growth_df.columns.to_list()
   exported["growth_df"] = export_single_dataframe(growth_df, "growth_df", growth_skip)
-  
+
   ratios_skip = ratios_df.columns.to_list()
   exported["ratios_df"] = export_single_dataframe(ratios_df, "ratios_df", ratios_skip)
-  
+
   val_skip = valuation_df.columns.to_list()
   exported["valuation_df"] = export_single_dataframe(valuation_df, "valuation_df",val_skip)
-  
+
   diag_skip = diagnostic_df.columns.to_list()
   exported["diagnostic_df"] = export_single_dataframe( diagnostic_df, "diagnostic_df", diag_skip)
-  
+
   results_df = results_df.reset_index()
   results_df.rename(columns={results_df.columns[0]: "Scenario"}, inplace=True)
   results_skip = results_df.columns.to_list()
   exported["results_df"] = export_single_dataframe(results_df, "results_df", results_skip)
-  
+
   valsum_skip = valuation_summary.columns.to_list()
   exported["valuation_summary"] = export_single_dataframe(valuation_summary, "valuation_summary", valsum_skip)
-  
+
   sensitivity_table = sensitivity_table.reset_index()
   sensitivity_table.rename(columns={sensitivity_table.columns[0]: "WACC"}, inplace=True)
   sensitivity_table = sensitivity_table.melt(
@@ -131,10 +131,10 @@ def export_for_tableau(
   score_df.columns = [col.replace('_x', '') for col in score_df.columns]
   score_skip = score_df.columns.to_list()
   exported["score_df"] = export_single_dataframe(score_df, "score_df", score_skip)
-  
+
   decision_skip = decision_df.columns.to_list()
   exported["decision_df"] = export_single_dataframe(decision_df, "decision_df", decision_skip)
-  
+
   driver_skip = driver_scenario_df.columns.to_list()
   exported["driver_scenario_df"] = export_single_dataframe(driver_scenario_df, "driver_scenario_df", driver_skip)
 
